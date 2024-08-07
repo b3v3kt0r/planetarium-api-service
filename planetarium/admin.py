ad24b3f82ce1cd7e8fcad4e3f513d7b9b8df719e
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-08-07 14:39:14 +0300

    initialize base models & update settings & update admin panel

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from planetarium.models import (
+    ShowSession,
+    ShowTheme,
+    AstronomyShow,
+    Reservation,
+    Ticket,
+    PlanetariumDome
+)
 
-# Register your models here.
+admin.site.register(ShowSession)
+admin.site.register(ShowTheme)
+admin.site.register(AstronomyShow)
+admin.site.register(Reservation)
+admin.site.register(Ticket)
+admin.site.register(PlanetariumDome)
--- planetarium/models.py
+++ planetarium/models.py
@@ -1,3 +1,39 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    show_theme = models.ForeignKey("ShowTheme", on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f"Name: {self.title}, description: {self.description}, Theme: {self.show_theme}"
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=63)
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=63)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE)
+    planetarium_dome = models.ForeignKey(PlanetariumDome, on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_created=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(ShowSession, on_delete=models.CASCADE, related_name="tickets")
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name="tickets")
