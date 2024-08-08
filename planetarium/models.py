544e444e1ec7b929b93ff4491306e22188ba6b45
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-08-08 14:01:55 +0300

    update models

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import UniqueConstraint
 
 
 class AstronomyShow(models.Model):
@@ -44,6 +45,9 @@ class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ["-created_at"]
+
     def __str__(self):
         return str(self.created_at)
 
@@ -54,5 +58,22 @@ class Ticket(models.Model):
     show_session = models.ForeignKey(ShowSession, on_delete=models.CASCADE, related_name="tickets")
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name="tickets")
 
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=["seat", "show_session"], name=["unique_ticket_seat_show_session"])
+        ]
+
     def __str__(self):
         return f"{self.show_session} (row: {self.row}, seat: {self.seat}"
+
+    def clean(self):
+        if not (1 <= self.seat <= self.show_session.planetarium_dome.seats_in_row):
+            raise ValueError({
+                "seat": f"seat must be in range [1, {self.show_session.planetarium_dome.seats_in_row}], not {self.seat}"
+            })
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(force_insert, force_update, using, update_fields)
