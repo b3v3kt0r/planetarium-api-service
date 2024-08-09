a72be1bed0f05f59e7543b401c1542ea02e8d599
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-08-09 17:04:03 +0300

    fixes in settings

--- planetarium_api_service/settings.py
+++ planetarium_api_service/settings.py
@@ -131,10 +131,10 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES":
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        ["rest_framework_simplejwt.authentication.JWTAuthentication"],
     "DEFAULT_PAGINATION_CLASS":
         "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 5,
     "DEFAULT_PERMISSION_CLASSES":
-        "rest_framework.permissions.IsAuthenticated",
+        ["rest_framework.permissions.IsAuthenticated"],
 }
