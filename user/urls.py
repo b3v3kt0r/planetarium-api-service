59e71c6bc51ce7a2754ea56c75907125fc1229ed
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-08-09 16:43:17 +0300

    add jwt verify route

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.authtoken import views
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from user.views import CreateUserView, LoginUserView
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
